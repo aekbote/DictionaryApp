5dc91b067df82413130f1e86406f9dd66b264721
aekbote <88759575+aekbote@users.noreply.github.com>
2022-08-31 13:56:36 -0700

    Update App.js

--- App.js
+++ App.js
@@ -3,22 +3,15 @@ import React from 'react';
 import { render } from 'react-dom';
 import { StyleSheet, Text, View } from 'react-native';
 
+import HomeScreen from './screens/HomeScreen.js';
+
 export default class App extends React.Component {
   render(){
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+     <HomeScreen/>
+     
     </View>
   );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
